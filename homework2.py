a8494dd490e3c63c2104ddbe37bbbdbc6cd5646d
apryMEHT-RnD <fantazer_rnd@inbox.ru>
2024-07-26 14:59:07 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,6 @@
+completed_tasks = 12
+hours_spent = 1.5
+сourse_name = "Python"
+time_for_one_task = hours_spent / completed_tasks
+print("Курс:", сourse_name, ",всего задач:", completed_tasks, ",затрачено часов:", hours_spent, ",среднее время выполения", hours_spent / completed_tasks, "часа.")
+
