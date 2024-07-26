fd2f0fdc7142d512433949ba2783eeb457b63f55
apryMEHT-RnD <fantazer_rnd@inbox.ru>
2024-07-26 17:46:31 +0300

    Add files via upload

---
+++ homework3.py
@@ -0,0 +1,13 @@
+name = "apryMEHT"
+print('Name:', name)
+age = 41
+print('Age:',age)
+new_age = 42
+print('New Age:', new_age)                            # этот вариант print('New Age:', age+1) также выводит 42
+is_student = True
+print("Is_Student:", is_student)
+
+
+
+
+
