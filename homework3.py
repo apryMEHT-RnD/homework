5b74ed520a6a847d7242a32bd55acdfcf2608b3d
apryMEHT-RnD <fantazer_rnd@inbox.ru>
2024-07-27 10:32:49 +0300

    Update homework3.py

--- homework3.py
+++ homework3.py
@@ -1,9 +1,9 @@
 name = "apryMEHT"
 print('Name:', name)
-age = 41
+age = 40
 print('Age:',age)
-new_age = 42
-print('New Age:', new_age)                            # этот вариант print('New Age:', age+1) также выводит 42
+age = 40 + 1
+print('New Age:', age)
 is_student = True
 print("Is_Student:", is_student)
 
