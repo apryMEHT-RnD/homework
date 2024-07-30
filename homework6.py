7dc5f5843988d3003d7df10fb8403ffdb2421501
apryMEHT-RnD <fantazer_rnd@inbox.ru>
2024-07-30 17:28:18 +0300

    Add files via upload

---
+++ homework6.py
@@ -0,0 +1,26 @@
+my_dict = {"Denis": 1983 , "Vova": 1995, "Anton": 2010 }
+print(my_dict)
+print(my_dict ["Denis"])
+print(my_dict .get("Max"))
+my_dict .update({"Danil": 2015, "Roma": 1975})
+print(my_dict .pop("Vova"))
+print(my_dict)
+
+
+my_self = {1, 2, 2, 3, 4, 5, 5.25, 5, 1, 2, 3, 3, 4, "string"}
+print(my_self)
+my_self .add((10, 100))
+my_self .discard(5)
+print(my_self)
+
+
+
+
+
+
+
+
+
+
+
+
