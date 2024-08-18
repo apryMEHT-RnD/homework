e7ff6df4dd1ab8129632624f08f753222851a1ef
apryMEHT-RnD <fantazer_rnd@inbox.ru>
2024-08-18 16:51:11 +0300

    Add files via upload

---
+++ module_4_1.py
@@ -0,0 +1,10 @@
+from fake_match import divide as FM
+from true_match import divide as TM
+result1 = FM(69, 3)
+result2 = FM(3, 0)
+result3 = TM(49, 7)
+result4 = TM(15, 0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
