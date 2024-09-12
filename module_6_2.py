59ad2dfd4609a4dba053d95ff0f3780caaf8de9a
apryMEHT-RnD <fantazer_rnd@inbox.ru>
2024-09-12 17:17:27 +0300

    Add files via upload

---
+++ module_6_2.py
@@ -0,0 +1,41 @@
+class Vehicle:
+    __COLOR_VARIANTS = ["Black", "White", "Red", "Green", "Bluee"]
+    def __init__(self, owner, __model, __engine_power, __color):
+        self.owner = str(owner)
+        self.__model = str(__model)
+        self.__engine_power = int(__engine_power)
+        self.__color = str(__color)
+
+    def get_model(self):
+        print(f"Модель: {self.__model}")
+
+    def get_horsepower(self):
+        print(f"Мощность двигателя: {self.__engine_power}")
+
+    def get_color(self):
+        print(f"Цвет: {self.__color.upper()}")
+
+    def print_info(self):
+        self.get_model()
+        self.get_horsepower()
+        self.get_color()
+        print(f"Владелец: {self.owner}\n")
+
+    def set_color(self, new_color: str):
+        if new_color.lower() in self.__COLOR_VARIANTS:
+            self.__color = new_color
+        else:
+            print(f'Нельзя сменить цвет на: {new_color}')
+
+class Sedan(Vehicle):
+    __PASSENGERS_LIMIT = 5  # (в седан может поместиться только 5 пассажиров)
+
+vehicle1 = Sedan('Fedos', 'Toyota Mark II', 500, 'blue')
+
+vehicle1.print_info()
+
+vehicle1.set_color('Pink')
+vehicle1.set_color('BLACK')
+vehicle1.owner = 'Vasyok'
+
+vehicle1.print_info()
