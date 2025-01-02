253c44b93192b659df3bb73da2dc45a503636eb6
apryMEHT-RnD <fantazer_rnd@inbox.ru>
2025-01-02 16:29:39 +0300

    Add files via upload

--- task.py
+++ task.py
@@ -1,28 +1,17 @@
-from fastapi import APIRouter
+from app.backend.db import Base
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
+from sqlalchemy.orm import relationship
+from app.models.user import User
 
-router = APIRouter(prefix='/task', tags=['task'])
+class Task(Base):
+    __tablename__ = 'tasks'
+    __table_args__ = {'extend_existing': True}
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String)
+    content = Column(String)
+    priority = Column(Integer, default=0)
+    completed = Column(Boolean, default=False)
+    user_id = Column(Integer, ForeignKey('users.id'), nullable=False, index=True)
+    slug = Column(String, unique=True, index=True)
 
-
-@router.get("/")
-async def all_tasks():
-    pass
-
-
-@router.get("/{task_id}")
-async def task_by_id(task_id: int):
-    pass
-
-
-@router.post("/create")
-async def create_task():
-    pass
-
-
-@router.put("/update")
-async def update_task():
-    pass
-
-
-@router.delete("/delete")
-async def delete_task():
-    pass
+    user = relationship('User', back_populates='tasks')
--- user.py
+++ user.py
@@ -1,28 +1,16 @@
-from fastapi import APIRouter
-
-router = APIRouter(prefix='/user', tags=['user'])
-
-
-@router.get("/")
-async def all_user():
-    pass
+from app.backend.db import Base
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
 
 
-@router.get("/{user_id}")
-async def user_by_id(user_id: int):
-    pass
+class User(Base):
+    __tablename__ = "users"
+    __table_args__ = {'keep_existing': True}
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String)
+    firstname = Column(String)
+    lastname = Column(String)
+    age = Column(Integer)
+    slug = Column(String, unique=True, index=True)
 
-
-@router.post("/create")
-async def create_user():
-    pass
-
-
-@router.put("/update")
-async def update_user():
-    pass
-
-
-@router.delete("/delete")
-async def delete_user():
-    pass
+    tasks = relationship('Task', back_populates='user')
